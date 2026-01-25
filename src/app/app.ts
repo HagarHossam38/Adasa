82eb9e8e11245d047452f77c0f866c8c3f499f9e
HagarHossam38 <hagarhossam610@gmail.com>
2026-01-25 22:43:33 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Hagar-HossamEldin-Saturday-7PM-Maadi-Assignment14-AdasaApp-01147722482');
+}
