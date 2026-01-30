91ee2a73b3c347704fc722691dca9f44c737c07b
HagarHossam38 <hagarhossam610@gmail.com>
2026-01-31 01:30:06 +0200

    changeRoutingTitles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { About } from './components/about/about';
@@ -6,8 +7,9 @@ import { Notfound } from './components/notfound/notfound';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: Home, title: 'Adasa|Home' },
-    { path: 'about', component: About, title: 'Adasa|About' },
-    { path: 'blog', component: Blog, title: 'Adasa|Blog' },
-    { path: '**', component: Notfound, title: 'Adasa|Failed' }
+    { path: 'home', component: Home, title: 'عدسة - عالم التوصير' },
+    { path: 'about', component: About, title: 'عدسة - من نحن' },
+    { path: 'blog', component: Blog, title: 'عدسة - المدونة' },
+    { path: '404', component: Notfound, title: 'خطأ' },
+    { path: '**', redirectTo: '404' }
 ];
