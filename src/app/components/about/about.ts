169420cba86b875e08ea694331642f356fedbc9e
HagarHossam38 <hagarhossam610@gmail.com>
2026-02-06 01:13:25 +0200

    addAboutHeader

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { GridBackground } from "../grid-background/grid-background";
+import { SectionLabel } from "../section-label/section-label";
+import { PingDot } from "../ping-dot/ping-dot";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [GridBackground, SectionLabel, PingDot],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
